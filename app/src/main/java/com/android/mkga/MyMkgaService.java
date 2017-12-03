41a2bfb0b675a23529cc9f66088539d855435776
Murad <muradmagomedow@gmail.com>
2017-12-03 16:09:43 +0400

    layout updates

--- app/src/main/java/com/android/mkga/MyMkgaService.java
+++ app/src/main/java/com/android/mkga/MyMkgaService.java
@@ -3,14 +3,21 @@ package com.android.mkga;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
+import java.util.List;
 import java.util.Map;
 
 import ru.evotor.framework.core.IntegrationService;
 import ru.evotor.framework.core.action.processor.ActionProcessor;
+import ru.evotor.framework.receipt.Position;
+import ru.evotor.framework.receipt.Receipt;
+import ru.evotor.framework.receipt.ReceiptApi;
 
 public class MyMkgaService extends IntegrationService {
 
     double checkSum;
+
+    List<Position> positions = ReceiptApi.getReceipt(this, Receipt.Type.SELL).getPositions();
+
 
     @Override
     public void onCreate() {
@@ -23,6 +30,7 @@ public class MyMkgaService extends IntegrationService {
     protected Map<String, ActionProcessor> createProcessors() {
         Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("currentCheckSum", checkSum);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         return null;
     }
