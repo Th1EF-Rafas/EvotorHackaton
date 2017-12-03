7c5ec0ddfb2a44035b165475b09416022a3a48aa
Murad <muradmagomedow@gmail.com>
2017-12-03 13:19:56 +0400

    not ready yet, just some scratches

--- app/src/main/java/com/android/mkga/MyMkgaService.java
+++ app/src/main/java/com/android/mkga/MyMkgaService.java
@@ -1,53 +1,30 @@
 package com.android.mkga;
 
-import android.os.RemoteException;
-import android.support.annotation.NonNull;
+import android.content.Intent;
 import android.support.annotation.Nullable;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import ru.evotor.framework.core.IntegrationService;
-import ru.evotor.framework.core.action.event.receipt.changes.position.IPositionChange;
-import ru.evotor.framework.core.action.event.receipt.changes.position.SetExtra;
-import ru.evotor.framework.core.action.event.receipt.discount.ReceiptDiscountEvent;
-import ru.evotor.framework.core.action.event.receipt.discount.ReceiptDiscountEventProcessor;
-import ru.evotor.framework.core.action.event.receipt.discount.ReceiptDiscountEventResult;
 import ru.evotor.framework.core.action.processor.ActionProcessor;
 
 public class MyMkgaService extends IntegrationService {
+
+    double checkSum;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        checkSum = 282.41;
+    }
+
     @Nullable
     @Override
     protected Map<String, ActionProcessor> createProcessors() {
-        Map<String, ActionProcessor> map = new HashMap<>();
-        map.put(ReceiptDiscountEvent.NAME_SELL_RECEIPT, new ReceiptDiscountEventProcessor() {
-            @Override
-            public void call(@NonNull String action, @NonNull ReceiptDiscountEvent event, @NonNull Callback callback) {
-                try {
-                    //Значение скидки на весь чек в рублях или иной валюте
-                    BigDecimal discount = new BigDecimal(10);
-                    JSONObject object = new JSONObject();
-                    object.put("someSuperKey", "AWESOME DISCOUNT");
-                    SetExtra extra = new SetExtra(object);
-                    List<IPositionChange> listOfChanges = new ArrayList<>();
-                    callback.onResult(
-                            new ReceiptDiscountEventResult(
-                                    discount,
-                                    extra,
-                                    listOfChanges
-                            ));
-                } catch (JSONException | RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        return map;
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("currentCheckSum", checkSum);
+        startActivity(intent);
+        return null;
     }
 }
 
