c98434f04ce0a6e6a574af82820e3d5fe368e343
Murad <muradmagomedow@gmail.com>
2017-12-03 16:23:40 +0400

    changes

--- app/src/main/java/com/android/mkga/MyMkgaService.java
+++ app/src/main/java/com/android/mkga/MyMkgaService.java
@@ -3,21 +3,14 @@ package com.android.mkga;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 
-import java.util.List;
 import java.util.Map;
 
 import ru.evotor.framework.core.IntegrationService;
 import ru.evotor.framework.core.action.processor.ActionProcessor;
-import ru.evotor.framework.receipt.Position;
-import ru.evotor.framework.receipt.Receipt;
-import ru.evotor.framework.receipt.ReceiptApi;
 
 public class MyMkgaService extends IntegrationService {
 
     double checkSum;
-
-    List<Position> positions = ReceiptApi.getReceipt(this, Receipt.Type.SELL).getPositions();
-
 
     @Override
     public void onCreate() {
